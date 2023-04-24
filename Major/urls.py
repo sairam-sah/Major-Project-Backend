c5fbfbc9f6a884bfba7477ca21d1b58812063806
Pratik Roshan Shah <pratik.roshan.21@gmail.com>
2023-04-24 08:41:34 +0545

    URLS Added

--- Major/urls.py
+++ Major/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from Apoth import views
+from Apoth.views import Flower_Lists
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Flower_Lists.as_view()),
     path('floinfo/<int:pk>', views.flower_detail),
     path('floinfo/', views.flower_list),
 ]
